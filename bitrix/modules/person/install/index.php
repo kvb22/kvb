f80a65426d399b867b1695fd9016c89e8e6d63c3
Kirill Bakhmetyev <rahja@narod.ru>
2016-02-20 08:55:48 +0300

    Создание таблиц базы данных

--- bitrix/modules/person/install/index.php
+++ bitrix/modules/person/install/index.php
@@ -43,14 +43,89 @@ return true;
 function DoInstall()
 {
 global $DOCUMENT_ROOT, $APPLICATION;
-$this->InstallFiles();
+if( $this->InstallDB() )
+{
+    $this->InstallFiles();
+}
 RegisterModule("person");
 $APPLICATION->IncludeAdminFile("Установка модуля Person", $DOCUMENT_ROOT."/bitrix/modules/person/install/step.php");
+}
+
+function InstallDB()
+{
+    
+    global $DB;
+ 
+     $sql = "
+ 
+DROP TABLE IF EXISTS `Person`;
+
+    ";
+    
+    $dbRes = $DB->Query($sql); 
+ 
+    $sql = "
+
+CREATE TABLE IF NOT EXISTS `Person` (
+  `id` int(11) NOT NULL,
+  `group_id` int(11) NOT NULL DEFAULT '0',
+  `first_name` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `last_name` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `gender` enum('f','m') COLLATE utf8_unicode_ci NOT NULL DEFAULT 'm'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+    ";
+    
+    $dbRes = $DB->Query($sql);    
+    
+    $sql = "
+    
+DROP TABLE IF EXISTS `Group`;
+
+    ";
+    
+    $dbRes = $DB->Query($sql);    
+    
+    $sql = "
+
+CREATE TABLE IF NOT EXISTS `Group` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) CHARACTER SET utf8 NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+    ";
+    
+    $dbRes = $DB->Query($sql);
+
+    $sql = "
+
+INSERT INTO `Group` (`id`, `title`) VALUES
+(1, 'Группа 1'),
+(2, 'Группа 2'),
+(3, 'Группа 3');    
+    
+    ";
+ 
+    $dbRes = $DB->Query($sql);
+    
+    return true;
+
+}
+
+ function UnInstallDB()
+ {
+    global $DB;     
+    $sql = "DROP TABLE IF EXISTS `Group`;";
+    $dbRes = $DB->Query($sql);
+    $sql = "DROP TABLE IF EXISTS `Person`;";
+    $dbRes = $DB->Query($sql);
+    return true;    
 }
 
 function DoUninstall()
 {
 global $DOCUMENT_ROOT, $APPLICATION;
+$this->UnInstallDB();
 $this->UnInstallFiles();
 UnRegisterModule("person");
 $APPLICATION->IncludeAdminFile("Деинсталляция модуля Person", $DOCUMENT_ROOT."/bitrix/modules/person/install/unstep.php");
